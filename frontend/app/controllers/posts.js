1d7def87478666017391a69b05e649d5167536d5
Piotr Bakalarski <piotrb5e3@gmail.com>
2016-10-02 19:52:27 +0100

    Fixed buggy post sorting

--- frontend/app/controllers/posts.js
+++ frontend/app/controllers/posts.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
+import moment from 'moment';
 
 export default Ember.Controller.extend({
+  sortedPosts: Ember.computed.sort('model', (a, b) => {
+    return moment(a.get('date')).isAfter(b.get('date')) ?
+      -1 :
+      1;
+  })
 });
--- frontend/app/routes/posts.js
+++ frontend/app/routes/posts.js
@@ -1,15 +1,7 @@
 import Ember from 'ember';
-import RSVP from 'rsvp';
 
 export default Ember.Route.extend({
   model() {
-    let self = this;
-    return new RSVP.Promise(function(resolve) {
-      self.get('store').findAll('post').then(function(model) {
-        resolve(model.toArray().sort((a, b) => {
-          return a.get('date') < b.get('date');
-          }));
-      });
-    });
+    return this.get('store').findAll('post');
   }
 });
