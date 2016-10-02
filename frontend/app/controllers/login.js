6857382b84345a7128efdcf021e20b5942fa1821
Piotr Bakalarski <piotrb5e3@gmail.com>
2016-10-02 23:02:24 +0100

    Improved login page. Changed new/edit post form layout

--- frontend/app/controllers/login.js
+++ frontend/app/controllers/login.js
@@ -2,7 +2,8 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   session: Ember.inject.service('session'),
-
+  identification: "",
+  password: "",
   actions: {
     authenticate() {
       let { identification, password } = this.getProperties('identification', 'password');
